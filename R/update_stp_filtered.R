8db3f372a75d5de972c139b813754762ee2563f9
rweberc <rweberc@gmail.com>
2023-12-27 22:49:26 -0500

    Continued clean up of todos

--- R/update_stp_filtered.R
+++ R/update_stp_filtered.R
@@ -18,21 +18,6 @@ update_stp_filtered <- function(df_name,
                                 project_dictionary,
                                 project_directory) {
 
-  # you know it's to save a mapping if ref_ob isn't null.... you know if there's an update to save if ref_ob isn't null (are there exception cases?)
-
-  # TODO: have option to read this, then make update and write it to directory, then return the copy to work from...
-
-  # TODO: check for existence of stp_ob (when update = TRUE)
-
-  # TODO: check stp_ob is well-formed (no NAs for ids)
-
-  # TODO: check for existence of row (only allow for complete overwrite or addition?)
-
-  # TODO: update when row id may also respect the data frame
-
-  # TODO: likely should update this function so that it just receives a list of values for add_item... then depending on the entity in the stp_ob,
-  #         creates the row and does some checking...
-
   # Try to read current mapping
   stp_ob = get_stp_object(project_dictionary,
                           dir = project_directory,
@@ -42,8 +27,6 @@ update_stp_filtered <- function(df_name,
   existing_rows = stp_ob$filter_items %>%
     dplyr::filter(stp_id == stp_id_var) %>%
     nrow()
-
-  # TODO: perhaps have function to do all things like checking for duplicates by id, etc.
 
   if (existing_rows > 0) {
 
@@ -60,8 +43,6 @@ update_stp_filtered <- function(df_name,
 
     }
   }
-
-  # TODO: add data-type checks for the input data?  must have stp_id_var... must have a ref_ob?
 
   compare_path = project_dictionary$compare_metadata_path
   has_update_ob = as.numeric(!is.null(update_ob))
@@ -90,19 +71,5 @@ update_stp_filtered <- function(df_name,
   saveRDS(stp_ob, file.path(project_directory, project_dictionary$current_metadata_path))
 
 }
-
-
-# maybe update this so every time it is reading it from the path.expand
-
-# first tries to read it from the path
-
-# if not there, attempts to read from the parent environment (+ warning)
-
-# if reads from the path, then necessarily writes to the path
-
-# if reads from the environment, then gives warning, and does not write to the path ... a lot more
-
-# have dictionary object to suppress warnings about modifying the object in the parent environment? (this would be to limit all the read/write tot he disk for each function call...)
-
 
 
--- R/update_stp_mappings.R
+++ R/update_stp_mappings.R
@@ -18,34 +18,15 @@ update_stp_mappings <- function(df_name,
                                 project_dictionary,
                                 project_directory) {
 
-  # TODO: have option to read this, then make update and write it to directory, then return the copy to work from...
-
-  # TODO: Possibly consider having default options for this... if default = TRUE... will only just fill in those values...
-  #         and can have default set to TRUE by default, but in the update_xyz_object calls... would be set to FALSE to prevent writing without sufficient values...
-
-  # TODO: check for existence of stp_ob (when update = TRUE)
-
-  # TODO: check stp_ob is well-formed (no NAs for ids)
-
-  # TODO: check for existence of row (only allow for complete overwrite or addition?)
-
-  # TODO: update when row id may also respect the data frame
-
-  # TODO: likely should update this function so that it just receives a list of values for add_item... then depending on the entity in the stp_ob,
-  #         creates the row and does some checking...
-
-
   # Try to read current mapping
   stp_ob = get_stp_object(project_dictionary,
                           dir = project_directory,
                           file_type = "current")
 
-  # check if stp_ob currently has a row for this id
+  # Check if stp_ob currently has a row for this id
   existing_rows = stp_ob$mapping_items %>%
     dplyr::filter(stp_id == stp_id_var) %>%
     nrow()
-
-  # TODO: perhaps have function to do all things like checking for duplicates by id, etc.
 
   if (existing_rows > 0) {
 
@@ -62,8 +43,6 @@ update_stp_mappings <- function(df_name,
 
     }
   }
-
-  # TODO: add data-type checks for the input data?  must have stp_id_var must have a ref_ob?
 
   compare_path = project_dictionary$compare_metadata_path
   has_update_ob = as.numeric(!is.null(update_ob))
@@ -91,19 +70,4 @@ update_stp_mappings <- function(df_name,
   saveRDS(stp_ob, file.path(project_directory, project_dictionary$current_metadata_path))
 
 }
-
-
-# maybe update this so every time it is reading it from the path.expand
-
-# first tries to read it from the path
-
-# if not there, attempts to read from the parent environment (+ warning)
-
-# if reads from the path, then necessarily writes to the path
-
-# if reads from the environment, then gives warning, and does not write to the path ... a lot more
-
-# have dictionary object to suppress warnings about modifying the object in the parent environment? (this would be to limit all the read/write tot he disk for each function call...)
-
-
 
