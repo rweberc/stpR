5617283f52910253d86b1f290efa472acc8946cf
rweberc <rweberc@gmail.com>
2023-12-29 14:48:40 -0500

    git updates

---
+++ secure_data/Note On This Folder.R
@@ -0,0 +1,3 @@
+# This folder is included in the project level folder of stpR to support testing and example use of the stp_ob.rds object for illustration purposes.
+
+# In practice, "secure_data" would be included in the .git_ignore folder of the project utilizing the stpR package to prevent this data from being versionized.
